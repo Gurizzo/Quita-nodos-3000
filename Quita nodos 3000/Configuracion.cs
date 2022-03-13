c787269ac77be91975dab36e7514eede59dfbc2f
Gustavo Rizzo <gurizzo2009@gmail.com>
2022-03-13 03:36:42 -0300

    Agregar archivos de proyecto.

---
+++ Quita nodos 3000/Configuracion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Quita_nodos_3000
+{
+    [XmlRoot(ElementName = "Configuracion")]
+    public class Configuracion
+    {
+        [XmlElement(ElementName = "DirectorioInicio")]
+        public string DirectorioInicio { get; set; }
+
+        [XmlElement(ElementName = "DirectorioFinal")]
+        public string DirectorioFinal { get; set; }
+
+        [XmlElement(ElementName = "Xpaths")]
+        public List<string> Xpaths { get; set; }
+
+        [XmlAttribute(AttributeName = "xsi")]
+        public string Xsi { get; set; }
+
+        [XmlAttribute(AttributeName = "xsd")]
+        public string Xsd { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+
+        internal void GuardarConfiguracion()
+        {
+            string directorio = AppDomain.CurrentDomain.BaseDirectory;
+            XmlSerializer ser = new XmlSerializer(typeof(Configuracion));
+
+            using (FileStream fs = new FileStream(directorio + "configuracion.xml", FileMode.Create))
+            {
+                ser.Serialize(fs, this);
+            }
+        }
+        
+        internal Configuracion CargarConfig()
+        {
+            string directorio = AppDomain.CurrentDomain.BaseDirectory;
+            XmlSerializer ser = new XmlSerializer(typeof(Configuracion));
+            string direccionArchivo = directorio + "configuracion.xml";
+            if (File.Exists(direccionArchivo))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(direccionArchivo);
+
+                using (StringReader reader = new StringReader(doc.OuterXml))
+                {
+                    return (Configuracion)ser.Deserialize(reader);
+                }
+            }
+            
+            return null;
+        }
+    }
+}
---
+++ Quita nodos 3000/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Quita_nodos_3000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CargarConfiguracion();
+        }
+
+        private void CargarConfiguracion()
+        {
+            Configuracion configuracion = new Configuracion();
+
+            var configGuardada = configuracion.CargarConfig();
+
+            textBoxFinal.Text = configGuardada.DirectorioFinal;
+            textBoxInicial.Text = configGuardada.DirectorioInicio;
+
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> list = new List<string>();
+            list.Add("pruebita");
+            Configuracion configuracion = new Configuracion()
+            {
+                DirectorioFinal = textBoxFinal.Text,
+                DirectorioInicio = textBoxInicial.Text,
+                Xpaths = list
+            };
+            configuracion.GuardarConfiguracion();
+
+        }
+        private void btnInicial_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            
+                dialog.ShowDialog();
+            textBoxInicial.Text = dialog.SelectedPath;
+        
+
+        }
+
+        private void btnFinal_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+
+            dialog.ShowDialog();
+            textBoxFinal.Text = dialog.SelectedPath;
+        }
+
+    }
+}
